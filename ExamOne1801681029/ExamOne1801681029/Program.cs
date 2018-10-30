7a587582fb05c31029820c1a680d2be13b06d22b
kadrie <kadrie_989@abv.bg>
2018-10-30 14:25:55 +0200

    Initial files uploaded!

---
+++ ExamOne1801681029/ExamOne1801681029/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExamOne1801681029
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
